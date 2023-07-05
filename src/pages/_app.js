a42c02bde29c727709117ea382c3ded51d243ee9
vongarcia97 <vongarcia97@gmail.com>
2023-07-05 17:25:20 -0400

    feat: intercept user refresh events

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,11 @@
 import '@/styles/globals.css';
 import { UserSocketProvider } from '@/context/UserSocketContext';
-import { useReloadPageConfirm } from '@/utils/hooks/useReloadPageConfirm';
+
+import useReloadPageConfirm from '@/utils/hooks/useReloadPageConfirm';
 
 export default function App({ Component, pageProps }) {
+
+  useReloadPageConfirm();
 
   return (
     <UserSocketProvider>
--- src/utils/hooks/useReloadPageConfirm.js
+++ src/utils/hooks/useReloadPageConfirm.js
@@ -1,6 +1,13 @@
 "use client";
 import { useEffect } from "react";
 
+import { useRouter } from 'next/router'
+
+/**
+ * 
+ * "use client";
+import { useEffect } from "react";
+
 export const useReloadPageConfirm = () => {
 
   useEffect(() => {
@@ -22,3 +29,27 @@ export const useReloadPageConfirm = () => {
     };
   }, [])
 };
+ */
+
+const useReloadPageConfirm = () => {
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      const confirmationMessage = 'Are you sure you want to refresh the page? You will automatically be kicked out of your table and lost all of your entries.';
+      // Check if the event is due to a page refresh
+      if (window.PerformanceNavigationTiming.type === "reload") {
+        event.preventDefault();
+        event.returnValue = confirmationMessage; // Display a confirmation message
+      }
+
+      return confirmationMessage; // This is necessary for some older browsers
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [])
+}
+export default useReloadPageConfirm
