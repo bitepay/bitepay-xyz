98a0d7d541a5582bf56d73836e07729ab7da2e93
vongarcia97 <vongarcia97@gmail.com>
2023-07-09 10:05:37 -0400

    fix: update socket connection domain to reflect AWS route53 changes

--- src/utils/hooks/useLeavePageConfirm.js
+++ src/utils/hooks/useLeavePageConfirm.js
@@ -1,19 +1,30 @@
-
+"use client"
 import { useEffect } from "react";
 
 export const useLeavePageConfirm = () => {
   useEffect(() => {
+
+    const isChrome = () => navigator.userAgent.includes('Chrome')
+
     const handler = (event) => {
       event.preventDefault()
       const message = "Are you sure want to leave this page? You will be kicked out of your table and your entries will be lost."
-      event.returnValue = message
-      return event
+      if (!isChrome) {
+        event.preventDefault()
+      } else {
+        console.log('userAgent has Chrome')
+  
+        event.returnValue = message
+  
+        return event
+
+      }
     }
 
-    window.addEventListener("beforeunload", handler, { capture: true })
+    window.addEventListener("beforeunload", handler)
 
     return () => {
-      window.removeEventListener("beforeunload", handler, { capture: true })
+      window.removeEventListener("beforeunload", handler)
     }
   }, [])
 }
